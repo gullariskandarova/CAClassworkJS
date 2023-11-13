165e83e5aac0a74a7ac987028732431862139997
iskandarofha <iskenderovaguller777@gmail.com>
2023-11-13 01:50:00 -0800

    array methods

---
+++ 07.Array&string-Methods/main.js
@@ -0,0 +1,28 @@
+//----------------------------------------------------- TASK 1 -----------------------------------------------------
+
+// findFirstNotRepeatedChar adlı bir function yaradın. Bu function göndərdiyiniz string-in ilk təkrarlanmayan
+// hərfini qaytarsın. Nəticə =====>> "e"
+
+// let findFirstNotRepeatedChar = (string) => {
+//   let newStr = "";
+//   for (let i = 0; i < string.lenght; i++) {
+//     string.indexOf(string[i]) === string.lastIndexOf(string[i]) ? (newStr += string[i]) : null;
+//   }
+
+//   return newStr[0];
+// }
+
+// console.log(findFirstNotRepeatedChar("abacddbecz"));
+
+// ----------------------------------------------------- TASK 2 -----------------------------------------------------
+
+// charCount adlı function yaradın. Bu function 2 ədəd argument qəbul edir. 2-ci göndərdiyiniz hərf 1-cinin içərisində
+// neçə dəfə təkrarlanıbsa onun sayısını göstərin.
+// Nəticə: 3
+// console.log(charCount('w3rescource.com', 'c'));
+
+let charCount = (resourse, char) => {
+    let count = 0;
+    for 
+
+};
