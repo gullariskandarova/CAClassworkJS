0af156494291bcf6d838b8d9b6a9cce0a258586c
iskandarofha <iskenderovaguller777@gmail.com>
2023-11-27 01:59:49 -0800

    Storage

--- 15.STORAGE/loginPage/storage.js
+++ 15.STORAGE/loginPage/storage.js
@@ -1,30 +1,24 @@
 let form = document.querySelector("form");
-let allInputs = document.querySelectorAll(".text");
-let users = JSON.parse(localStorage.getItem("users")) || [];
+let allInputs = document.querySelectorAll("input");
+console.log(allInputs);
+
+let data = JSON.parse(localStorage.getItem("users")) || [];
+
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  let bool = users.some(
-    (item) =>
-      item.firstName === allInputs[0].value ||
-      item.lastName === allInputs[1].value
-  );
-  if (!bool) {
-    let userObj = {
-      firstName: allInputs[0].value,
-      lastName: allInputs[1].value,
-      address: allInputs[2].value,
-      id: Date.now(),
-    };
-    users.push(userObj);
-
-    localStorage.setItem("users", JSON.stringify(users));
-  } else {
-    window.alert("username or email already used");
+  
+  let info = {
+    id:Date.now(),
+    firstName: allInputs[0].value,
+    lastName: allInputs[1].value,
+    address: allInputs[2].value
   }
 
-  allInputs.forEach((item) => {
-    item.value = "";
-  });
+  data.push(info)
+  localStorage.setItem("users", JSON.stringify(info))
+  allInputs[0].value = "";
+  allInputs[1].value = "";
+  allInputs[2].value = ""
 
-  window.location = "about.html";
 });
