3c9e39e9dd6137b201c02dec3ae4fc1772a2e3a7
[iskandarofha] <[iskenderovaguller777@gmail.com]>
2023-11-29 06:28:31 +0400

    shopping basket

--- 16.BASKET/product.js
+++ 16.BASKET/product.js
@@ -316,18 +316,39 @@ const products = [
 ];
 
 let card = document.querySelector(".card");
+let cards = document.querySelector(".cards");
+let btnAdd = document.querySelector(".add-btn");
 drowCard(products);
 function drowCard(data) {
   data.forEach((item) => {
-    card.innerHTML = `
-        <img src="${item.thumbnail}" alt="" />
+    cards.innerHTML += `
+    <div class="card" style = "width: 30%; border: 1px solid; padding: 5px " >
+    <img style= "width: 100%" src="${item.thumbnail}" alt="" />
         <h4>${item.title}</h4>
-        <h4></h4>
-        <p></p>
-        <div class="btnIcon">
-            <button class="btn-add">Add to basket</button>
-            <i class="fa-regular fa-heart"></i>
-        </div>
+        <h4>${item.price}</h4>
+        <p>${item.description.slice(0, 100)}...</p>
+        <div class="btnIcon" style="display: flex; justify-content: space-between; color: red ">
+            <button class="btn-add" style = "padding: 10px 20px; border-radius: 25px; border: none; background-color: blue" onclick = "addBasket()">Add to basket</button>
+            <i class="fa-regular fa-heart" onclick = "iconFunc()"></i>
+            </div>
+    </div>
         `;
   });
 }
+
+function addBasket() {
+  
+}
+function iconFunc() {
+  this.classList.toggle("fa-solid");
+}
+let basket = document.querySelector(".basket");
+
+cards.style.display = "flex";
+cards.style.flexWrap = "wrap";
+cards.style.gap = "30px";
+
+// basket.addEventListener("click", function () {
+//   console.log("salam");
+//   window.location= "basket.html";
+// });
