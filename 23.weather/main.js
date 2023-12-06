d33b93e823b2c53fe3e3d2f1c110408e515035e6
[iskandarofha] <[iskenderovaguller777@gmail.com]>
2023-12-06 20:44:23 +0400

    weather program is completed

--- 23.weather/main.js
+++ 23.weather/main.js
@@ -1,9 +1,15 @@
+let addCity = document.querySelector("form");
+let city = document.querySelector("input");
 let curCity = "Ganja";
+
 let BASE_URL = `https://api.weatherapi.com/v1/current.json?key=de4fcc1d06a64857be7101727222212&q=${curCity}&aqi=no`;
 const dataW = document.querySelector(".dataWeather");
+getWeather("baku");
 
-function getWeather() {
-  fetch(`${BASE_URL}`)
+function getWeather(curCity) {
+  fetch(
+    `https://api.weatherapi.com/v1/current.json?key=de4fcc1d06a64857be7101727222212&q=${curCity}&aqi=no`
+  )
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
@@ -17,6 +23,10 @@ function getWeather() {
     `;
     });
 }
-getWeather();
 
-// Form.submit.addevent
+addCity.addEventListener("submit", function (event) {
+  event.preventDefault();
+  curCity = city.value;
+  getWeather(curCity)
+  
+});
